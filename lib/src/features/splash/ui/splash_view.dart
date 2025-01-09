735514df326dd24125739a07cadff69683f0cb07
Idriss Nono <36419248+Shadows-Master@users.noreply.github.com>
2025-01-09 16:16:49 -0700

    add background image on splashview

--- lib/src/features/splash/ui/splash_view.dart
+++ lib/src/features/splash/ui/splash_view.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:news/src/commons/constants/app_sizes.dart';
 import 'package:news/src/commons/constants/app_strings.dart';
+import 'package:news/src/commons/extensions/app_context_extension.dart';
 import 'package:news/src/commons/extensions/app_widget_extension.dart';
 import 'package:news/src/commons/widgets/components/app_title.dart';
 import 'package:news/src/commons/widgets/components/buttons/app_button.dart';
 import 'package:news/src/core/routing/app_router.dart';
+
+import '../../../../generated/assets.gen.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -14,28 +17,39 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     //final image = Assets.images.news;
     return Scaffold(
-      body: Column(
-        spacing: AppSize.p14,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Spacer(),
-          AppTitle.h2(
-            title: "Stay Informed from Day One".toUpperCase(),
-            fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black.withValues(alpha: 1),
+          image: DecorationImage(
+            image: AssetImage(Assets.images.news.path),
+            fit: BoxFit.cover,
+            opacity: .5,
           ),
-          AppTitle(
-            title: AppStrings.splashSubtitle,
-            maxLines: 3,
-            textAlign: TextAlign.center,
-          ).paddingSymmetric(horizontal: AppSize.p8),
-
-          AppButton(
-            title: "Getting Started",
-            onPressed: () => context.goNamed(AppRoutes.news.name),
-          ),
-          AppSize.p32.heightBox,
-        ],
+        ),
+        child: Column(
+          spacing: AppSize.p14,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            AppTitle.h2(
+              title: "Stay Informed from Day One".toUpperCase(),
+              fontWeight: FontWeight.bold,
+              color: context.surface,
+            ),
+            AppTitle(
+              title: AppStrings.splashSubtitle,
+              maxLines: 3,
+              textAlign: TextAlign.center,
+              color: context.surface,
+            ).paddingSymmetric(horizontal: AppSize.p8),
+            AppButton.secondary(
+              title: "Getting Started",
+              onPressed: () => context.goNamed(AppRoutes.news.name),
+            ),
+            AppSize.p32.heightBox,
+          ],
+        ),
       ),
     );
   }
